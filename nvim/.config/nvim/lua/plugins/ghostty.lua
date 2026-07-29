513e7d55e7ccd4c277d138cd6de8a2adcaf92e3f
ShanaNyaa <ShanaNyaa@users.noreply.github.com>
2026-07-30 00:23:49 +0800

    Add "ghostty.lua" plugin for NeoVim/LazyVim

---
+++ nvim/.config/nvim/lua/plugins/ghostty.lua
@@ -0,0 +1,8 @@
+-- Provides syntax highlighting for Ghostty config file
+-- No additional dependencies are required.
+
+return {
+  "ghostty",
+  dir = "/Applications/Ghostty.app/Contents/Resources/vim/vimfiles/",
+  lazy = false,
+}
