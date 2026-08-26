e55ff618f5efd6432bb200c2c3552f17bca084da
ShanaNyaa <ShanaNyaa@users.noreply.github.com>
2026-08-26 12:21:49 +0800

    Fix Noctalia's blur implementation being overwritten by Hyprland's blur

--- hypr/.config/hypr/config/windowrules.lua
+++ hypr/.config/hypr/config/windowrules.lua
@@ -205,3 +205,15 @@ hl.window_rule({
 	},
 	no_focus = true,
 })
+
+-- Enable blur support for Noctalia's bar, panels, dock, and notifications
+hl.layer_rule({
+	name = "noctalia",
+	match = {
+		namespace = "^noctalia-(bar-.+|notification|dock|panel|attached-panel|osd|window-switcher)$",
+	},
+	no_anim = true,
+	ignore_alpha = 0.5,
+	blur = true,
+	blur_popups = true,
+})
