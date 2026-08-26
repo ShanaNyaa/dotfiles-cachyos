c10459ea4e551999995170a34ba83924c426eb7d
ShanaNyaa <ShanaNyaa@users.noreply.github.com>
2026-08-26 12:33:06 +0800

    Add layerrules.lua and moved layer_rule implementation in windowrules.lua to layerrules.lua

--- hypr/.config/hypr/hyprland.lua
+++ hypr/.config/hypr/hyprland.lua
@@ -12,6 +12,7 @@ require("config.misc")
 require("config.monitors")
 require("config.windowrules")
 require("config.workspaces")
+require("config.layerrules")
 
 -- HyprMod managed settings
 require("hyprland-gui")
